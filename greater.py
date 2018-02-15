3d5f8b91e0de2a97636b2db084b3f701bf416d4a
praveena2mca <36407730+praveena2mca@users.noreply.github.com>
2018-02-15 10:34:48 +0530

    greater.py

---
+++ greater.py
@@ -0,0 +1,9 @@
+x=int(input("enter a number:"))
+y=int(input("enter a number:"))
+z=int(input("enter a number:"))
+if(x>y):
+    print("x is greater")
+elif(y>z):
+    print("y is greater")
+else:
+    print("z is greater")
